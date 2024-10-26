ca92756ef2fe58c22266e70e6ed6dff6e19deb00
Sango <letusgoone@outlook.com>
2024-10-26 16:21:55 +0800

    修复 format 丢失

--- Sources/Configurations.cpp
+++ Sources/Configurations.cpp
@@ -7,28 +7,33 @@
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <Cango/CommonUtils/Configurations.hpp>
-#include <frozen/map.h>
+#include <map>
 
-namespace {
+namespace
+{
 	using namespace Cango;
 	using namespace boost;
 	using namespace boost::property_tree;
 	using namespace boost::system;
 	using namespace boost::filesystem;
 
-	std::string ExtensionToLower(const std::string& extension) noexcept {
+	std::string ExtensionToLower(const std::string& extension) noexcept
+	{
 		static auto to_lower = [](const unsigned char c) noexcept { return std::tolower(c); };
 
-		std::string extension_lower{extension};
+		std::string extension_lower{ extension };
 		std::ranges::transform(extension, extension_lower.begin(), to_lower);
 		return extension_lower;
 	}
 
-	bool VerifyPathToRead(spdlog::logger& logger, const FilePathType& path) noexcept {
+	bool VerifyPathToRead(spdlog::logger& logger, const FilePathType& path) noexcept
+	{
 		const auto& path_string = path.string();
 
-		if (error_code result; !exists(path, result)) {
-			if (result.failed()) {
+		if (error_code result; !exists(path, result))
+		{
+			if (result.failed())
+			{
 				logger.error("检查路径({})是否存在时发生错误：{}", path_string, result.what());
 				return false;
 			}
@@ -36,8 +41,10 @@ namespace {
 			return false;
 		}
 
-		if (error_code result; !is_regular_file(path, result)) {
-			if (result.failed()) {
+		if (error_code result; !is_regular_file(path, result))
+		{
+			if (result.failed())
+			{
 				logger.error("检查路径({})是否为普通文件时发生错误：{}", path_string, result.what());
 				return false;
 			}
@@ -50,9 +57,10 @@ namespace {
 
 	using reader_t = std::function<void(const std::string&, VariableTable&)>;
 
-	std::optional<reader_t> SelectReader(spdlog::logger& logger, const FilePathType& path) noexcept {
+	std::optional<reader_t> SelectReader(spdlog::logger& logger, const FilePathType& path) noexcept
+	{
 #define sc_generate_reader(boost_call) [](const std::string& filename, VariableTable& table) noexcept { boost_call(filename, table); }
-		static const frozen::map<std::string, reader_t, 4> reader_map{
+		static std::map<std::string, reader_t> reader_map{
 			{".ini", sc_generate_reader(read_ini)},
 			{".json", sc_generate_reader(read_json)},
 			{".xml", sc_generate_reader(read_xml)},
@@ -63,7 +71,8 @@ namespace {
 		const auto extension = path.extension();
 		const auto& extension_string = extension.string();
 		const auto extension_lower = ExtensionToLower(extension_string);
-		if (!reader_map.contains(extension_lower)) {
+		if (!reader_map.contains(extension_lower))
+		{
 			logger.error("不支持的配置文件后缀：{}", extension_string);
 			return std::nullopt;
 		}
@@ -76,35 +85,41 @@ namespace {
 		const FilePathType& path,
 		VariableTable& table,
 		const reader_t& reader) noexcept
-	try {
+		try
+	{
 		reader(path.string(), table);
 		return true;
 	}
-	catch (const std::exception& ex) {
+	catch (const std::exception& ex)
+	{
 		logger.error("无法读取文件({}): {}", path.string(), ex.what());
 		return false;
 	}
-	catch (...) {
+	catch (...)
+	{
 		logger.error("无法读取文件({}): 未知错误", path.string());
 		return false;
 	}
 
-	bool CreateConfigFile(spdlog::logger& logger, const FilePathType& path) {
-		if (const ofstream stream{path, fstream::out}; stream.is_open()) return true;
+	bool CreateConfigFile(spdlog::logger& logger, const FilePathType& path)
+	{
+		if (const ofstream stream{ path, fstream::out }; stream.is_open()) return true;
 		logger.error("无法创建文件({})", path.string());
 		return false;
 	}
 
-	bool VerifyPathToWrite(spdlog::logger& logger, const FilePathType& path) noexcept {
+	bool VerifyPathToWrite(spdlog::logger& logger, const FilePathType& path) noexcept
+	{
 		if (error_code result{}; exists(path, result) && !result.failed()) return true;
 		return CreateConfigFile(logger, path);
 	}
 
 	using writer_t = std::function<void(const std::string&, const VariableTable&)>;
 
-	std::optional<writer_t> SelectWriter(spdlog::logger& logger, const FilePathType& path) noexcept {
+	std::optional<writer_t> SelectWriter(spdlog::logger& logger, const FilePathType& path) noexcept
+	{
 #define sc_generate_writer(boost_call) [](const std::string& filename, const VariableTable& pt) noexcept { boost_call(filename, pt); }
-		static const frozen::map<std::string, writer_t, 4> writer_map{
+		static std::map<std::string, writer_t> writer_map{
 			{".ini", sc_generate_writer(write_ini)},
 			{".json", sc_generate_writer(write_json)},
 			{".xml", sc_generate_writer(write_xml)},
@@ -115,7 +130,8 @@ namespace {
 		const auto extension = path.extension();
 		const auto& extension_string = extension.string();
 		const auto extension_lower = ExtensionToLower(extension_string);
-		if (!writer_map.contains(extension_lower)) {
+		if (!writer_map.contains(extension_lower))
+		{
 			logger.error("不支持的配置文件后缀：{}", extension_string);
 			return std::nullopt;
 		}
@@ -127,38 +143,46 @@ namespace {
 		const FilePathType& path,
 		const VariableTable& table,
 		const writer_t& writer) noexcept
-	try {
+		try
+	{
 		writer(path.string(), table);
 		return true;
 	}
-	catch (const std::exception& ex) {
+	catch (const std::exception& ex)
+	{
 		logger.error("无法写入配置文件({}): {}", path.string(), ex.what());
 		return false;
 	}
-	catch (...) {
+	catch (...)
+	{
 		logger.error("无法写入配置文件({}): 未知错误", path.string());
 		return false;
 	}
 }
 
-namespace Cango :: inline CommonUtils :: inline Configurations {
+namespace Cango :: inline CommonUtils :: inline Configurations
+{
 	bool LoadVariableTableFromFile(
 		spdlog::logger& logger,
 		VariableTable& table,
-		const FilePathType& file) noexcept {
+		const FilePathType& file) noexcept
+	{
 		const auto& path_string = file.string();
-		if (!VerifyPathToRead(logger, file)) {
+		if (!VerifyPathToRead(logger, file))
+		{
 			logger.error("无法读取配置文件({})", path_string);
 			return false;
 		}
 
 		const auto reader = SelectReader(logger, file);
-		if (!reader) {
+		if (!reader)
+		{
 			logger.error("找不到合适读取器读取配置文件({})", path_string);
 			return false;
 		}
 
-		if (!TryRead(logger, file, table, *reader)) {
+		if (!TryRead(logger, file, table, *reader))
+		{
 			logger.error("读取配置文件({})的内容时出现错误", path_string);
 			return false;
 		}
@@ -169,21 +193,25 @@ namespace Cango :: inline CommonUtils :: inline Configurations {
 	bool SaveVariableTableToFile(
 		spdlog::logger& logger,
 		const VariableTable& table,
-		const FilePathType& file) noexcept {
+		const FilePathType& file) noexcept
+	{
 		const auto& path_string = file.string();
 
-		if (!VerifyPathToWrite(logger, file)) {
+		if (!VerifyPathToWrite(logger, file))
+		{
 			logger.error("无法写入配置文件({})", path_string);
 			return false;
 		}
 
 		const std::optional<writer_t> writer = SelectWriter(logger, file);
-		if (!writer) {
+		if (!writer)
+		{
 			logger.error("找不到合适的写入器写入配置文件({})", path_string);
 			return false;
 		}
 
-		if (!TryWrite(logger, file, table, *writer)) {
+		if (!TryWrite(logger, file, table, *writer))
+		{
 			logger.error("写入内容到配置文件({})时出现异常", path_string);
 			return false;
 		}
